dfb5b419a6139a2b666f9f03d5cbf00a27fd9137
woldemb1 <woldemb1@aalto.fi>
2018-10-06 18:54:58 +0300

    updating screen mode

---
+++ ImageGrid/app/src/main/java/com/bereket/imagegrid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.bereket.imagegrid;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+    private class DownloadFile extends AsyncTask<URL, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(URL... urls) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+        }
+    }
+}
--- ScreenMode/app/src/main/java/com/bereket/screenmode/MainActivity.java
+++ ScreenMode/app/src/main/java/com/bereket/screenmode/MainActivity.java
@@ -1,13 +1,56 @@
 package com.bereket.screenmode;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.VolumeShaper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+     TextView prTv=null;
+     TextView landTv=null;
+    Switch swithch=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        prTv= findViewById(R.id.textPortrait);
+        landTv= findViewById(R.id.textLandscape);
+
+        swithch = (Switch)findViewById(R.id.scrChange);
+
+        if(Configuration.ORIENTATION_LANDSCAPE ==
+                this.getResources().getConfiguration().orientation){
+            prTv.setText("");
+            landTv.setText("Screen mode is set to landscape");
+
+        }else if (Configuration.ORIENTATION_PORTRAIT ==
+                this.getResources().getConfiguration().orientation){
+            prTv.setText("Screen mode is set to portrait");
+            landTv.setText("");
+
+        }
+
+        swithch.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked==true){
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                        }else{
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                        }
+                    }
+                }
+        );
     }
+
 }
